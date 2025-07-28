262b892ff4d9999daf979c3d7b8f370535b399bc
github mandarin1977 <pkm30106@gmail.com>
2025-07-28 22:26:04 +0900

    fdsdf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mydashbord/', // <-- GitHub 저장소 이름
+  base: '/mydashbord/',
   plugins: [react()],
 })
