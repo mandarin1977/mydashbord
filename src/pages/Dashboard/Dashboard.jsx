144eade270aa076482bcc62600fe9322b6596c93
mandarin1977 <57694287+mandarin1977@users.noreply.github.com>
2025-07-30 18:49:34 +0900

    Add files via upload

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import './Dashboard.scss';
 
 function Dashboard() {
     return (
         <>
-            <section>
-                <article>
+            <section className="mainDashboard">
+                <article className="noticeArea">
                     <aside>
                         <h2>공지사항</h2>
                         <p>여기다가 공지사항 내용 나옴</p>
+                        <button>더보기</button>
                     </aside>
                 </article>
-                <div>
-                    sdfsdf
-                </div>
+                <section className="mainDashArea">
+                    <article className="dashInfo">
+                        <ul className="dashInfoTab">
+                            <li>업무현황</li>
+                            <li>직원 관리</li>
+                            <li>일정&공지</li>
+                            <li>설정</li>
+                        </ul>
+                    </article>
+                </section>
             </section>
             
         </>
